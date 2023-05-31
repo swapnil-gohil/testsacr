9c4daf2bce1bb85f9fe40f3a4939c64aa5e679fd
Swapnil Gohil <67471384+swapnil-gohil@users.noreply.github.com>
2023-05-31 18:24:24 +0530

    Create enable_acr_access.ps1

---
+++ .github/scripts/enable_acr_access.ps1
@@ -0,0 +1,5 @@
+$Ip1=(Invoke-WebRequest -uri "http://ifconfig.me/ip" -DisableKeepAlive -UseBasicParsing).Content
+If(!([string]::IsNullOrEmpty($Ip1))){$githubPublicIp=$Ip1}
+$Ip2=(Invoke-WebRequest -uri "https://api.ipify.org/" -DisableKeepAlive -UseBasicParsing).Content
+If(!([string]::IsNullOrEmpty($Ip2))){$githubPublicIp=$Ip2}
+az acr network-rule add -n $env:REGISTRY_NAME --ip-address $githubPublicIp
